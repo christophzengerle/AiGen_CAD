462036b1d224fca1d75d0b45ff9ada49b2aa99f6
christophzengerle <christoph.zengerle90@gmail.com>
2024-05-02 22:08:58 +0200

    minor fix

--- DeepCAD/exec_commands.py
+++ DeepCAD/exec_commands.py
@@ -2,7 +2,7 @@ import os
 
 # cmd = "python pc2cad.py --exec train --exp_name pcEncoder --batch_size 150 --num_workers 1 -g 0"
 
-cmd = "python pc2cad.py --exec test --mode dec --pc_root evaluation/eval_pc_cad_images/source \
+cmd = "python pc2cad.py --exec test --mode dec --pc_root evaluation/eval_pc_cad_images/source/00777945.ply \
     --exp_name pcEncoder --ckpt latest --ae_exp_name pretrained --ae_ckpt ckpt_epoch1000 \
     --n_points 4096 --expSTEP --expPNG --expGIF -g 0"
     
