a84b798b1d2f1805cd2eb02c19fc369f553df1d4
Jorge Mandlmaier <jorge.mandlmaier@gmail.com>
2024-05-24 14:37:34 +0200

    docker

--- InstantMesh/test.py
+++ InstantMesh/test.py
@@ -5,10 +5,8 @@ import torch
 
 model = InstantMesh()
 
-m = trimesh.load("../data/mesh_files/00000007.obj")
+m = trimesh.load("../utils/data/mesh_files/00000007.obj")
 
 device = torch.device('cuda')
 model.init_flexicubes_geometry(device=device)
-model.render_mesh(mesh_v=m.vertices, mesh_f=m.faces, cam_mv=np.identity(4))
-
-sudo cp /var/cuda-repo-ubuntu2204-11-8-local/cuda-*-keyring.gpg /usr/share/keyrings/
+mask, hard_mask, tex_pos, depth, normal = model.render_mesh(mesh_v=m.vertices, mesh_f=m.faces, cam_mv=np.identity(4))
